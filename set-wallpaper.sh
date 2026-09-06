b3f705e5f0e2c9e1f257ea4ef37e23fe6174a52a
Rohan2CorE <isuke553@gmail.com>
2026-09-06 14:51:38 +0530

    Add files via upload

--- set-wallpaper.sh
+++ set-wallpaper.sh
@@ -458,6 +458,42 @@ cat > "$CONFIG_DIR/xfwm4.xml" << 'WMEOF'
 </channel>
 WMEOF
 
+# ---- REMOVE DEFAULT BOTTOM XFCE PANEL (Keep only top menu bar) -----
+cat > "$CONFIG_DIR/xfce4-panel.xml" << 'PANELXML'
+<?xml version="1.0" encoding="UTF-8"?>
+<channel name="xfce4-panel" version="1.0">
+  <property name="configver" type="int" value="2"/>
+  <property name="panels" type="array">
+    <value type="int" value="1"/>
+  </property>
+  <property name="panel-1" type="empty">
+    <property name="position" type="string" value="p=6;x=0;y=0"/>
+    <property name="length" type="uint" value="100"/>
+    <property name="position-locked" type="bool" value="true"/>
+    <property name="size" type="uint" value="28"/>
+    <property name="plugin-ids" type="array">
+      <value type="int" value="1"/>
+      <value type="int" value="2"/>
+      <value type="int" value="3"/>
+      <value type="int" value="4"/>
+      <value type="int" value="5"/>
+    </property>
+  </property>
+  <property name="plugins" type="empty">
+    <property name="plugin-1" type="string" value="applicationsmenu"/>
+    <property name="plugin-2" type="string" value="separator">
+      <property name="expand" type="bool" value="true"/>
+      <property name="style" type="uint" value="0"/>
+    </property>
+    <property name="plugin-3" type="string" value="systray"/>
+    <property name="plugin-4" type="string" value="clock">
+      <property name="digital-format" type="string" value="%a %b %d  %I:%M %p"/>
+    </property>
+    <property name="plugin-5" type="string" value="showdesktop"/>
+  </property>
+</channel>
+PANELXML
+
 # ---- PERMANENT XFCE DESKTOP XML CONFIG -----------------------------
 chattr -i "$CONFIG_DIR/xfce4-desktop.xml" 2>/dev/null || true
 cat > "$CONFIG_DIR/xfce4-desktop.xml" << XMLEOF
